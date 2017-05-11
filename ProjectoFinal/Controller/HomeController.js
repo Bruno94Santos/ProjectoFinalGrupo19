8f78e55c807708d239a7edb416a69ad20cdcddee
DanielaCSantos <danycostasantos@gmail.com>
2017-05-11 11:35:08 +0100

    New Project
    Version 1

---
+++ ProjectoFinal/Controller/HomeController.js
@@ -0,0 +1,70 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('HomeController', HomeController);
+
+    HomeController.$inject = ['$rootScope','$scope','$http'];
+    function HomeController($rootScope, $scope, $http) {
+        var vm = this;
+
+       /* vm.user = null;
+        vm.allUsers = [];
+        vm.deleteUser = deleteUser;
+
+        initController();
+
+        function initController() {
+            loadCurrentUser();
+            loadAllUsers();
+        }
+
+        function loadCurrentUser() {
+            UserService.GetByUsername($rootScope.globals.currentUser.username)
+                .then(function (user) {
+                    vm.user = user;
+                });
+        }
+
+        function loadAllUsers() {
+            UserService.GetAll()
+                .then(function (users) {
+                    vm.allUsers = users;
+                });
+        }
+
+        function deleteUser(id) {
+            UserService.Delete(id)
+            .then(function () {
+                loadAllUsers();
+            });
+        }
+        $scope.upload = function () {
+          $http.post('upload.ashx',$scope.files,{headers:{'Content-Type':'multipart/form-data'}})
+          .success(funtion(d))
+          {console.log(d)};
+        }
+        $scope.filesChanged = function(elm){
+          $scope.files = elm.files
+          $scope.$apply();
+        }
+
+        $scope.openNav = function() {
+            console.log("AQUi");
+            document.getElementById("mySidenav").style.width = "250px";
+            document.getElementById("main").style.marginLeft = "250px";
+            document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+          }
+          $scope.asdf = function() {
+              console.log("AQUi");
+            }
+*/
+      /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+       /* $scope.closeNav = function() {
+            document.getElementById("mySidenav").style.width = "0";
+            document.getElementById("main").marginLeft = "0";
+            document.body.style.backgroundColor = "white";
+          }
+        }*/
+})();
