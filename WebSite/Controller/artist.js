fcab1ba8d9822648cd03cc4fd1e24e3ccbf7b99c
SusanaVieira15 <susana.andre.vieira@gmail.com>
2017-05-12 20:58:15 +0100

    não está acabado. é para o tiago

---
+++ WebSite/Controller/artist.js
@@ -0,0 +1,32 @@
+/**
+ * Created by susana on 12/05/2017.
+ */
+var app = angular.module("myapp",[]);
+app.controller("artist", function($scope, $http){
+    $http.get('../Model/artist.php', {'artist_id': $scope.idProfile})
+        .success(function(result){
+            $scope.profile = result;
+        })
+        .error(function(err){
+        });
+
+
+    $scope.pageMusic = 1;
+    $http.get('../Model/artist.php', {'page': $scope.pageMusic})
+        .success(function(result){
+            $scope.music = result;
+        })
+        .error(function(err){
+        });
+    $scope.showMusic = function ( ) {
+        $scope.pageMusic += 1;
+        $http.get('../Model/artist.php', {'page': $scope.pageMusic})
+            .success(function(result){
+                $scope.music = result;
+            })
+            .error(function(err){
+            });
+
+    };
+
+});
---
+++ WebSite/Model/artist.php
@@ -0,0 +1,65 @@
+<?php
+ include "../../inc/dbinfo.inc";
+
+
+ session_start(); //on index top
+ if(!isset($_SESSION["loggedin"])){
+	$_SESSION["loggedin"]=0;
+ }
+
+ header("Content-Type: application/json; charset=UTF-8");
+
+ $connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
+
+ function get_artist($artist_id){
+		$result = $conn->query("SELECT * FROM artists WHERE artist_id = $artist_id");
+		if($result){
+			if (mysqli_num_rows($result)>0){
+				$linha=mysqli_fetch_assoc($result);
+				$output="";
+				$output .= '{"ArtistUserID":"'  . $linha["id"] . '",';
+				$output .= '"ArtistID":"'  . $linha["artist_id"] . '",';
+				$output .= '"ArtistPicture":"'  . $linha["picture"] . '",';
+				$output .= '"ArtistName":"'  . $linha["name"] . '",';
+				$output .= '"ArtistLocation":"'  . $linha["location"] . '",';
+				$output .= '"ArtistDescription":"'  . $linha["description"] . '",';
+				$output .= '"NumberRatings":"'. $linha["rating_n"] . '",';
+				$output .= '"SumRatings":"'. $linha["rating_sum"] . '",';
+				$output .= '"ArtistRating":"'. $linha["rating_n"]/$linha["rating_sum"] . '"}';
+				return $output;
+			}
+			else{
+				echo "There was an error fetching this artist. This page might have been deleted.";
+			}
+		}
+		else{
+			echo "Could not establish connection.";
+		}
+	}
+
+
+	function get_media_by_artist($id){
+    		$result = $conn->query("SELECT * FROM media WHERE artist = $id");
+    		if($result){
+    			while($linha = $result->fetch_array(MYSQLI_ASSOC)) {
+    				if ($output != "") {$output .= ",";}
+    				$output="";
+    				$output .= '{"MediaID":"'  . $linha["id"] . '",';
+    				$output .= '"ArtistID":"'  . $linha["artist"] . '",';
+    				$output .= '"IsSong":"'  . $linha["song"] . '",';
+    				$output .= '"Description":"'  . $linha["description"] . '",';
+    				$output .= '"Media":"'  . $linha["media"] . '",';
+    				$output .= '"NumberRatings":"'. $linha["rating_n"] . '",';
+    				$output .= '"SumRatings":"'. $linha["rating_sum"] . '",';
+    				$output .= '"MediaRating":"'. $linha["rating_n"]/$linha["rating_sum"] . '"}';
+    				$output ='{"records":['.$output.']}';
+    			return $output;
+    			}
+    			else{
+    				echo "Cannot find media. Media might have been deleted.";
+    			}
+    		}
+    		else{
+    			echo "Could not establish connection.";
+    		}
+    	}
