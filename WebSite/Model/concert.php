bc9fcd3a322fe67812a80736cf5f876c83e5fe5b
NixenSya <NixenSya@users.noreply.github.com>
2017-05-16 11:17:41 +0100

    Update concert.php

--- WebSite/Model/concert.php
+++ WebSite/Model/concert.php
@@ -18,6 +18,7 @@
  			while($linha = $result->fetch_array(MYSQLI_ASSOC)) {
  				if ($output != "") {$output .= ",";}
  				$output .= '{"EventID":"'  . $linha["id"] . '",';
+				$output .= '"EventCreator":"'  . $linha["creator_id"] . '",';
  				$output .= '"EventName":"'  . $linha["event_name"] . '",';
  				$output .= '"EventTime":"'  . $linha["event_time"] . '",';
  				$output .= '"SoldOut":"'  . $linha["sold_out"] . '",';
