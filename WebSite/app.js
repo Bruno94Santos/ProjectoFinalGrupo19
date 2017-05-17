88692aa8274a0c8b2547903c46d0c646e8869979
DanielaCSantos <danycostasantos@gmail.com>
2017-05-17 15:32:30 +0100

    almost

--- WebSite/app.js
+++ WebSite/app.js
@@ -2,16 +2,16 @@ use strict';
 var app = angular.module("myapp",['ngRoute']);
 app.config(function($routeProvider){
 $routeProvider
-  .when('/View',{
-    templateUrl: 'View/home.html',
+	.when('/View',{
+		templateUrl: 'View/home.html',
 		controller: 'HomeController'			
-		})
-		.when('/View', {
-			templateUrl: 'View/login.html',
-			controller: 'HomeController'
-		})
-		.when('/View', {
-			templateUrl: 'View/register.html',
-			controller: 'register'
-		});
+	})
+	.when('/View', {
+		templateUrl: 'View/login.html',
+		controller: 'HomeController'
+	})
+	.when('/View', {
+		templateUrl: 'View/register.html',
+		controller: 'register'
+	});
 });
