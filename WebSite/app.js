39182d81e4af5a80b142a4af5368a26baf7e8e72
DanielaCSantos <danycostasantos@gmail.com>
2017-05-17 15:30:07 +0100

    New update working

--- WebSite/app.js
+++ WebSite/app.js
@@ -1,28 +1,19 @@
-'use strict';
-
-var app = angular.module("myapp",['ngRoute']);
-
-app.config(function($routeProvider, $locationProvider){
-	$routeProvider
-		.when('/',{
-			templateUrl: 'View/home.html',
-			controller: 'home'
-		})
-		.when('/login', {
-			templateUrl: 'View/login.html',
-			controller: 'login'
-		})
-		.when('/register', {
-			templateUrl: 'View/register.html',
-			controller: 'register'
-		})
-		.when('/concert', {
-			templateUrl: 'View/concert.html',
-			controller: 'concert'
-		})
-		.when('/dashboard', {
-			templateUrl: 'View/dashboard.html',
-			controller: 'dashboard'
-		});
-	$locationProvider.html5Mode(true);
-});
+use strict';
++
++var app = angular.module("myapp",['ngRoute']);
++
++app.config(function($routeProvider){
++	$routeProvider
++		.when('/View',{
++			templateUrl: 'View/home.html',
++			controller: 'HomeController'			
++		})
++		.when('/View', {
++			templateUrl: 'View/login.html',
++			controller: 'HomeController'
++		})
++		.when('/View', {
++			templateUrl: 'View/register.html',
++			controller: 'register'
++		});
++});
