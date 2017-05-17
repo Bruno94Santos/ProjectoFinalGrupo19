495889a19b5e0e49226357b87b401ff89f45ffd1
DanielaCSantos <danycostasantos@gmail.com>
2017-05-17 11:14:25 +0100

    Isto

--- WebSite/Model/login.php
+++ WebSite/Model/login.php
@@ -12,22 +12,20 @@ header("Content-Type: application/json; charset=UTF-8");
  
  $database = mysqli_select_db($connection, DB_DATABASE);
 	if($_SESSION["loggedin"]==0){
-		$result = $connection->query("SELECT * FROM users WHERE username='".$username."' OR email='".$username."' AND PASSWORD='".$password."');
+		$result = $connection->query("SELECT * FROM users WHERE username=".$username." OR email=".$username." AND PASSWORD=".$password.";");
 
 		if($result){
 			if (mysqli_num_rows($result)>0){
 				$linha=mysqli_fetch_assoc($result);
-				session = {'username':$username, "id":$linha['id'], "picture":$linha["picture"], "email":$linha["email"], "is_artist":$linha["is_artist"]};
-				$_SESSION["user"]=session;
-				/*$_SESSION["username"]=$username;
+				//session = {'username':$username, "id":$linha['id'], "picture":$linha["picture"], "email":$linha["email"], "is_artist":$linha["is_artist"]};
+				$_SESSION["username"]=$username;
 				$_SESSION["id"]=$linha['id'];
 				$_SESSION["picture"]=$linha["picture"];
 				$_SESSION["email"]=$linha["email"];
 				$_SESSION["bio"]=$linha["bio"];
 				$_SESSION["is_artist"]=$linha["is_artist"];
 				$_SESSION["loggedin"]=1;
-				$_SESSION["admin"]=0;*/
-				echo session;
+				$_SESSION["admin"]=0;
 			}
 			else{
 				echo "Username/email does not exist.";
