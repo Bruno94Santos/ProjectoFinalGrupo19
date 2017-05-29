dfc967daddaec3283277711e0ad8c09a99e7b33a
SusanaVieira15 <susana.andre.vieira@gmail.com>
2017-05-29 16:54:31 +0100

    parte java da SIDENAV adicionada a todos os Controllers

--- WebSite/Controller/concert.js
+++ WebSite/Controller/concert.js
@@ -20,6 +20,19 @@ app.controller("concert", function($scope, $http){
 
 });
 
+/**********************SIDE BAR / MENU */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").marginLeft = "0";
+    document.body.style.backgroundColor = "white";
+}
 
 
 
